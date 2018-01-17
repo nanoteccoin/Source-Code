eda186cc58ba992253d1e7efa9670cb380c8a2c2
Yugandhar Gangu <nanotechnologycoin@gmail.com>
2018-01-17 13:39:03 +0530

    Source code

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define NANOTEC-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "NANOTECURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
